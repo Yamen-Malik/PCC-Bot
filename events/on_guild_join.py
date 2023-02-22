23737c32a8ff5869550d697df39bcb507a44a469
Yamen Malik <60931606+Yamen-Malik@users.noreply.github.com>
2023-02-23 02:10:58 +0300

    Fixed on guild join event
    Changes:
        - Used deep copy to copy the default guild data

--- events/on_guild_join.py
+++ events/on_guild_join.py
@@ -2,10 +2,11 @@ from discord.ext import commands
 from discord import Guild
 from replit import db
 from constants import DEFAULT_GUILD_DATA
+from copy import deepcopy
 
 
 @commands.Cog.listener()
 async def on_guild_join(guild: Guild):
-    db[str(guild.id)] = DEFAULT_GUILD_DATA
+    db[str(guild.id)] = deepcopy(DEFAULT_GUILD_DATA)
 
 exported_events = [on_guild_join]
