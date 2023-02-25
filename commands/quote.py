0d6925d1b52588bafb978d0c1c1a8d29ad53a027
Yamen Malik <60931606+Yamen-Malik@users.noreply.github.com>
2023-02-25 18:04:20 +0300

    Changed the quotes api to quotable.io
    Changes:
        - Changed quote command to quotable.io since the old api is down
        - Added a tag parameter to quote command
        - Added api credits to all quotes
        - Added cooldown on quote command

--- commands/quote.py
+++ commands/quote.py
@@ -9,32 +9,44 @@ class Quote(Cog):
 
 
     @app_commands.command(name="quote")
-    async def quote(self, interaction: Interaction, id: str = "random") -> None:
+    @app_commands.checks.cooldown(150, 60)
+    async def quote(self, interaction: Interaction, id: str = "random", tag: str = None) -> None:
         """Send a programming related quote by it's id, if id isn't provided return random quote
 
         Args:
-            id (str, optional): id of the requested quote. Defaults to random
+            id (str): id of the requested quote. Defaults to random
+            tag (str): tag of the random quote. Defaults to None
         """
-
+        api_url = "https://api.quotable.io"
         try:
             # send GET request to programming quotes api
-            content = requests.get(
-                f"https://programming-quotes-api.herokuapp.com/Quotes/{id}")
+            if id == "random":
+                endpoint = "/random"
+                if tag:
+                    endpoint += f"?tags={tag}" 
+            else:
+                endpoint = f"/quotes/{id}"
+
+            content = requests.get(api_url + endpoint)
             if content.status_code == 404:
-                await interaction.response.send_message("Invalid quote id", ephemeral=True)
-                return False
+                if id == "random":
+                    message = "Invalid quote tag!"
+                else:
+                    message = "Invalid quote id!"
+                
+                await interaction.response.send_message(message, ephemeral=True)
+                return
             elif content.status_code != 200:
                 raise Exception
 
             # load response and send it's content in an Embed object
             content = content.json()
             quote = Embed(
-                title=content["author"], description=content["en"]+"\n\n"+content["id"], color=Color.gold())
+                title=content["author"], description=f"{content['content']}\n\nid: {content['_id']}", color=Color.gold())
+            quote.add_field(name="",value="Powered by [quotable.io](https://quotable.io)", inline=False)
             await interaction.response.send_message(embed=quote)
-            return True
         except:
-            await interaction.response.send_message("Error while requesting quote", ephemeral=True)
-            return False
+            await interaction.response.send_message("Error while requesting quote!", ephemeral=True)
 
 
 async def setup(bot: Bot) -> None:
