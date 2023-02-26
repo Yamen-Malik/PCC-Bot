445f7e140521d94bd2b61bc42a8f487f62ad8e51
Yamen Malik <60931606+Yamen-Malik@users.noreply.github.com>
2023-02-26 16:30:15 +0300

    Added quote tags command
    Changes:
        - Added tags command to Quote class that sends all available tags
        - Changed Quote Class from a Cog to a Group
        - Renamed quote command to random
        - Added on_error callback to the Quote class

--- commands/quote.py
+++ commands/quote.py
@@ -1,67 +1,115 @@
 from discord import app_commands, Interaction, Embed, Color
-from discord.ext.commands import Cog, Bot
+from discord.ext.commands import Bot
 import requests
 from logging import getLogger
 
-class Quote(Cog):
+class Quote(app_commands.Group):
+    """Interact with the quotes API
+    """
+
+    quote_api_url = "https://api.quotable.io"
+    images_api_url = "https://images.quotable.dev/profile"
+    
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.logger = getLogger(__name__)
+        super().__init__()
 
 
-    @app_commands.command(name="quote")
+    @app_commands.command()
     @app_commands.checks.cooldown(50, 60)
-    async def quote(self, interaction: Interaction, tag: str = None) -> None:
+    async def random(self, interaction: Interaction, tag: str = None) -> None:
         """Send a random quote
 
         Args:
             tag (str): tag of the random quote. Defaults to None
         """
 
-        quote_api_url = "https://api.quotable.io"
-        images_api_url = "https://images.quotable.dev/profile"
+        # send GET request to quotes api
+        endpoint = "/random"
+        if tag:
+            endpoint += f"?tags={tag}" 
+        
+        quote_response = requests.get(self.quote_api_url + endpoint)
+        if quote_response.status_code == 404:
+            await interaction.response.send_message("Invalid quote tag!", ephemeral=True)
+            return
+        elif quote_response.status_code != 200:
+            raise app_commands.AppCommandError(quote_response.json())
+
+        
+        # load quote response content
+        quote_content = quote_response.json()
+        
+        # send GET request to get the author's official or wikipedia page link
+        author_link = None
         try:
-            # send GET request to quotes api
-            endpoint = "/random"
-            if tag:
-                endpoint += f"?tags={tag}" 
-            
-            quote_response = requests.get(quote_api_url + endpoint)
-            if quote_response.status_code == 404:
-                await interaction.response.send_message("Invalid quote tag!", ephemeral=True)
-                return
-            elif quote_response.status_code != 200:
-                raise Exception
-
-            
-            # load quote response content
-            quote_content = quote_response.json()
-            
-            # send GET request to get the author's official or wikipedia page link
-            author_link = None
-            try:
-                authors_response = requests.get(f"{quote_api_url}/authors?slug={quote_content['authorSlug']}")
-                authors_content = authors_response.json()
-                author_link = authors_content["results"][0]["link"]
-            except Exception as e:
-                self.logger.warning(f"Failed to retrieve author data due to the following exception:\n{e}")
-                
-
-            # create and send quote embed
-            quote = Embed(description=quote_content['content'], color=Color.gold())
-            
-            # author image and link
-            quote.set_thumbnail(url=f"{images_api_url}/200/{quote_content['authorSlug']}.jpg")
-            quote.set_author(name=quote_content["author"], url=author_link)
-            
-            # credit 
-            quote.add_field(name="",value="Powered by [quotable.io](https://quotable.io)", inline=False)
-
-            await interaction.response.send_message(embed=quote)
+            authors_response = requests.get(f"{self.quote_api_url}/authors?slug={quote_content['authorSlug']}")
+            authors_content = authors_response.json()
+            author_link = authors_content["results"][0]["link"]
         except Exception as e:
-            self.logger.warning(e)
-            await interaction.response.send_message("Error while requesting quote!", ephemeral=True)
+            self.logger.warning(f"Failed to retrieve author data due to the following exception:\n{e}")
+            
+
+        # create and send quote embed
+        quote = Embed(description=quote_content['content'], color=Color.gold())
+        
+        # author image and link
+        quote.set_thumbnail(url=f"{self.images_api_url}/200/{quote_content['authorSlug']}.jpg")
+        quote.set_author(name=quote_content["author"], url=author_link)
+        
+        # credit 
+        quote.add_field(name="",value="Powered by [quotable.io](https://quotable.io)", inline=False)
+
+        await interaction.response.send_message(embed=quote)
+    
+
+    @app_commands.command()
+    @app_commands.checks.cooldown(2, 60)
+    async def tags(self, interaction: Interaction) -> None:
+        """Send a list of available quote tags sorted in descending order 
+            relative to the count of quotes
+        """
+        
+        # send GET request to quotes api
+        endpoint = "/tags"
+        
+        tags_response = requests.get(self.quote_api_url + endpoint)
+        if tags_response.status_code != 200:
+            raise app_commands.AppCommandError(tags_response.json())
+
+        
+        # load tags response content
+        tags_content = tags_response.json()
+
+        # sort tags in descending order
+        tags_content = sorted(tags_content, key=lambda x: x["quoteCount"], reverse=True)
+        
+        # load and format tag names
+        tags = list(map(lambda x: x["name"], tags_content))
+        tags = ", ".join(tags)
+
+        # create and send quote embed
+        quote = Embed(title="Tags", description=tags, color=Color.brand_green())
+        
+        # credit 
+        quote.add_field(name="",value="Powered by [quotable.io](https://quotable.io)", inline=False)
+
+        await interaction.response.send_message(embed=quote)
+
+
+
+    async def on_error(self, interaction: Interaction, error: app_commands.AppCommandError) -> None:
+        """A callback that is called when a child's command raises an AppCommandError
+
+        Args:
+            interaction (Interaction): The interaction that is being handled.
+            error (app_commands.AppCommandError): the exception that was raised.
+        """
+
+        self.logger.warning(error)
+        await interaction.response.send_message("Error while requesting data from the quote API!", ephemeral=True)
 
 
 async def setup(bot: Bot) -> None:
-    await bot.add_cog(Quote(bot))
+    bot.tree.add_command(Quote(bot))
